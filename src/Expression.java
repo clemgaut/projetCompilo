c8fadfffe9a587b33a15edecc1a60e45b3850ff0
Alexis Boyer <alexis.boyer@insa-rennes.fr>
2013-02-28 17:02:20 +0100

    Ajout de la methode depilerType et modification du code interne a la methode faireOperation

--- src/Expression.java
+++ src/Expression.java
@@ -16,6 +16,12 @@ public class Expression implements Constante{
 		pileType.push(Type);
 	}
 	
+	public int depilerType()	
+	{
+		return pileType.pop();
+	}
+	
+	
 	public void empilerOperateur(int Operateur)
 	{
 		pileOperateur.push(Operateur);
@@ -25,49 +31,49 @@ public class Expression implements Constante{
 		return pileType.peek();
 	}
 	
-	//controle des types pour Addition, Soustraction, Multiplication et Division
+	//controle des types pour l'addition, la soustraction, la multiplication et la division
 	public void controlerTypeASMD (int type1, int type2, int operateur)
 	{
 		if(type1 == Yaka.ENTIER && type2 == Yaka.ENTIER)
 		{
-			pileType.push(Yaka.ENTIER);
+			empilerType(Yaka.ENTIER);
 		}
 		else
 		{
-			pileType.push(Yaka.ERREUR);
+			empilerType(Yaka.ERREUR);
 		}
 			
 	}
 	
-	//controle des types pour superieur, inferieur, superieur ou egal et inferieur ou egal
+	//controle des types pour les operateurs superieur, inferieur, superieur ou egal et inferieur ou egal
 	public void controlerTypeCompar(int type1, int type2, int operateur)
 	{
 		if(type1 == Yaka.ENTIER && type2 == Yaka.ENTIER)
 		{
-			pileType.push(Yaka.BOOLEEN);
+			empilerType(Yaka.BOOLEEN);
 		}
 		else
 		{
-			pileType.push(Yaka.ERREUR);
+			empilerType(Yaka.ERREUR);
 		}
 			
 	}
 	
-	//controle des types pour egal ou different
+	//controle des types pour les operateurs egal ou different
 	public void controlerTypeEgal(int type1, int type2, int operateur)
 	{
 		if(type1 == Yaka.ENTIER && type2 == Yaka.ENTIER)
 		{
-			pileType.push(Yaka.BOOLEEN);
+			empilerType(Yaka.BOOLEEN);
 		}
 		else if(type1 == Yaka.BOOLEEN && type2 == Yaka.BOOLEEN)
 		{
-			pileType.push(Yaka.BOOLEEN);
+			empilerType(Yaka.BOOLEEN);
 
 		}
 		else
 		{
-			pileType.push(Yaka.ERREUR);
+			empilerType(Yaka.ERREUR);
 		}
 			
 	}
@@ -78,12 +84,12 @@ public class Expression implements Constante{
 		
 		if(type1 == Yaka.BOOLEEN && type2 == Yaka.BOOLEEN)
 		{
-			pileType.push(Yaka.BOOLEEN);
+			empilerType(Yaka.BOOLEEN);
 
 		}
 		else
 		{
-			pileType.push(Yaka.ERREUR);
+			empilerType(Yaka.ERREUR);
 		}
 			
 	}
@@ -94,28 +100,28 @@ public class Expression implements Constante{
 		
 		if(type == Yaka.BOOLEEN)
 		{
-			pileType.push(Yaka.BOOLEEN);
+			empilerType(Yaka.BOOLEEN);
 
 		}
 		else
 		{
-			pileType.push(Yaka.ERREUR);
+			empilerType(Yaka.ERREUR);
 		}
 			
 	}
 	
-	//controle des types pour l'operateur negation
+	//controle des types pour l'operateur de negation
 	public void controlerTypeNeg(int type, int operateur)
 	{
 		
 		if(type == Yaka.ENTIER)
 		{
-			pileType.push(Yaka.ENTIER);
+			empilerType(Yaka.ENTIER);
 
 		}
 		else
 		{
-			pileType.push(Yaka.ERREUR);
+			empilerType(Yaka.ERREUR);
 		}
 			
 	}
@@ -129,97 +135,97 @@ public class Expression implements Constante{
 	
 		case Yaka.PLUS:
 			Yaka.yvm.iadd();	
-			type1 = pileType.pop();
-			type2 = pileType.pop();		
+			type1 = depilerType();
+			type2 = depilerType();		
 			controlerTypeASMD(type1, type2,Yaka.PLUS);
 			break;
 			
 		case Yaka.MOINS:
 			Yaka.yvm.isub();
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeASMD(type1, type2,Yaka.MOINS);
 			break;
 			
 		case Yaka.MUL:
 			Yaka.yvm.imul();
-			type1 = pileType.pop();
-			type2 = pileType.pop();				
+			type1 = depilerType();
+			type2 = depilerType();				
 			controlerTypeASMD(type1, type2,Yaka.MUL);
 			break;
 			
 		case Yaka.DIV:
 			Yaka.yvm.idiv();
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeASMD(type1, type2,Yaka.DIV);
 			break;
 			
 		case Yaka.OU:
 			Yaka.yvm.ior();	
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeEtOu(type1, type2, Yaka.OU);
 			break;
 			
 		case Yaka.ET:
 			Yaka.yvm.iand();
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeEtOu(type1, type2, Yaka.ET);
 			break;
 			
 		case Yaka.NON:
 			Yaka.yvm.inot();
-			type1=pileType.pop();	
+			type1=depilerType();	
 			controlerTypeNon(type1, Yaka.NON);
 			break;
 					
 		case Yaka.SUP:
 			Yaka.yvm.isup();
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeCompar(type1, type2, Yaka.SUP);
 			break;
 			
 		case Yaka.INF:
 			Yaka.yvm.iinf();
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeCompar(type1, type2, Yaka.INF);
 			break;
 			
 		case Yaka.SUPE:
 			Yaka.yvm.isupegal();
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeCompar(type1, type2, Yaka.SUPE);
 			break;
 			
 		case Yaka.INFE:
 			Yaka.yvm.iinfegal();
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeCompar(type1, type2, Yaka.INFE);
 			break;
 			
 		case Yaka.EGAL:
 			Yaka.yvm.iegal();
-			type1 = pileType.pop();
-			type2 = pileType.pop();	
+			type1 = depilerType();
+			type2 = depilerType();	
 			controlerTypeEgal(type1, type2, Yaka.EGAL);
 			break;
 			
 		case Yaka.DIFF:
 			Yaka.yvm.idif();
-			type1 = pileType.pop();
-			type2 = pileType.pop();			
+			type1 = depilerType();
+			type2 = depilerType();			
 			controlerTypeEgal(type1, type2, Yaka.DIFF);
 			break;
 			
 		case NEG:
 			Yaka.yvm.ineg();
-			type1= pileType.pop();
+			type1= depilerType();
 			controlerTypeNeg(type1, NEG);
 			
 			
